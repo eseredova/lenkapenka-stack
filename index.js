fd980e4c550289fa6c79d57ef3173c49b4f1d69c
Elena Seredova <e.grishch@gmail.com>
2016-11-11 21:01:13 +0300

    Using class insteaf of prototype

--- index.js
+++ index.js
@@ -1,31 +1,68 @@
 'use strict';
 
-function Stack () {
-    this.stack = [];
+
+
+class Stack {
+
+
+    /**
+     * Creates a new Stack
+     * @constructor
+     * @this {Stack}
+     */
+    constructor() {
+        this.stack = [];
+    }
+
+
+    /**
+     * @function
+     * @param {object} value any object that you want to store in a stack
+     * @summary Adds a new element onto the top of the stack
+     */
+    push(value) {
+        this.stack.push(value);
+    }
+
+
+    /**
+     * Checks if stack is empty
+     * @function
+     * @return {boolean} true if stack is empty, false otherwise
+     * @summary Checks if the stack is empty or not
+     */
+    isEmpty() {
+        return this.stack.length === 0;
+    }
+
+    /**
+     * @function
+     * @return {object} value of the top element of the stack
+     * @throws Will throw an error if stack is empty
+     * @summary Removes the element from the top of the stack
+     */
+    pop() {
+        if (this.stack.length === 0) {
+            throw new Error('Empty stack!');
+        } else {
+            return this.stack.pop();
+        }
+    }
+
+    /**
+     * @function
+     * @return {object} value of the top element of the stack
+     * @throws Will throw an error if stack is empty
+     * @summary Returns the element from the top of the stack
+     */
+    peek() {
+        if (this.stack.length === 0) {
+            throw new Error('Empty stack!');
+        } else {
+            return this.stack[this.stack.length - 1];
+        }
+    }
 }
 
-Stack.prototype.push = function(value) {
-    this.stack.push(value);
-};
-
-Stack.prototype.isEmpty = function() {
-    return this.stack.length === 0;
-};
-
-Stack.prototype.pop = function () {
-    if (this.stack.length === 0) {
-        throw new Error('Empty stack!');
-    } else {
-        return this.stack.pop();
-    }
-};
-
-Stack.prototype.peek = function() {
-    if (this.stack.length === 0) {
-        throw new Error('Empty stack!');
-    } else {
-        return this.stack[this.stack.length - 1];
-    }
-};
 
 module.exports = Stack;
