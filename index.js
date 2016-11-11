7bf461e26c5eadd3e4eed874c3eeb41d28320170
Elena Seredova <e.grishch@gmail.com>
2016-11-11 12:31:28 +0300

    style improvements, eslint added

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
 'use strict';
 
 function Stack () {
-  this.stack = []
+    this.stack = [];
 }
 
 Stack.prototype.push = function(value) {
-  this.stack.push(value);
-}
+    this.stack.push(value);
+};
 
 Stack.prototype.isEmpty = function() {
-  return this.stack.length === 0;
-}
+    return this.stack.length === 0;
+};
 
 Stack.prototype.pop = function () {
-  if (this.stack.length === 0) {
-    console.log("Empty stack!");
-  } else {
-    return this.stack.pop();
-  }
-}
+    if (this.stack.length === 0) {
+        console.log('Empty stack!;');
+    } else {
+        return this.stack.pop();
+    }
+};
 
 Stack.prototype.peek = function() {
-  if (this.stack.length === 0) {
-    console.log("Empty stack!");
-  } else {
-    return this.stack[this.stack.length - 1];
-  }
-}
+    if (this.stack.length === 0) {
+        console.log('Empty stack!');
+    } else {
+        return this.stack[this.stack.length - 1];
+    }
+};
 
 module.exports = Stack;
--- test.spec.js
+++ test.spec.js
@@ -1,23 +1,23 @@
-"use strict";
+'use strict';
 
 const assert = require('assert');
 const Stack = require('./index.js');
 
 
 describe('Stack tests', function () {
-  it('should be a function', function () {
-    assert.strictEqual(typeof Stack, 'function', 'Stack is not a function');
-  });
+    it('should be a function', function () {
+        assert.strictEqual(typeof Stack, 'function', 'Stack is not a function');
+    });
 
-  it('should be empty by default', function () {
-     const stack = new Stack();
-     assert.ok(stack.isEmpty(), 'New stack is not empty!');
-  });
+    it('should be empty by default', function () {
+        const stack = new Stack();
+        assert.ok(stack.isEmpty(), 'New stack is not empty!');
+    });
 
-  it('should not be empty after push', function () {
-    const stack = new Stack();
-    stack.push(1);
-    assert.ifError(stack.isEmpty(), 'Stack is empty!');
-  });
-  
+    it('should not be empty after push', function () {
+        const stack = new Stack();
+        stack.push(1);
+        assert.ifError(stack.isEmpty(), 'Stack is empty!');
+    });
+
 });
